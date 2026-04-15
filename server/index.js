73fde3c20adc12c60167f12707fbe3d125147ad0
Will Hamnett <willhamnett@Wills-MacBook-Air-2.local>
2026-04-14 20:06:48 -0400

    force server change

--- server/index.js
+++ server/index.js
@@ -194,3 +194,5 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
 }
 app.listen(PORT, '0.0.0.0', () => console.log('Triangulate running on port ' + PORT));
+
+// v2
